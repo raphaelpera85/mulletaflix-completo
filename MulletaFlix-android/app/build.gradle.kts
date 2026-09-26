00c182b5c23061e40aec14a71978a3ff659d7f12
raphaelpera85 <raphaelpera85@users.noreply.github.com>
2026-09-26 00:53:46 -0300

    chore(release): bump Android app to v1.3.73 (versionCode 374)

--- MulletaFlix-android/app/build.gradle.kts
+++ MulletaFlix-android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "org.mulletaflix.android"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 373
+        versionCode = 374
         versionName = libs.versions.appVersion.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
