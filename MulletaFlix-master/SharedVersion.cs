767552f8cdbbb9125417797917eb1257f3e8a888
raphaelpera85 <raphaelpera85@users.noreply.github.com>
2026-09-26 06:03:11 -0300

    chore(release): bump server version to 12.0.95

--- MulletaFlix-master/SharedVersion.cs
+++ MulletaFlix-master/SharedVersion.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 
-[assembly: AssemblyVersion("12.0.94")]
-[assembly: AssemblyFileVersion("12.0.94")]
+[assembly: AssemblyVersion("12.0.95")]
+[assembly: AssemblyFileVersion("12.0.95")]
